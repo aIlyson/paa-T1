ab477f87feb8606377748b7ac223ae4757e0f70b
Maju <maria.julia.2@ufpi.edu.br>
2026-09-13 22:57:35 -0300

    Implementação da  Treesort com insercao em BST e percurso feito em-ordem

--- src/tree_sort.c
+++ src/tree_sort.c
@@ -1,11 +1,65 @@
 #include "../include/tree_sort.h"
-#include <stdio.h>
+#include <stdlib.h>
 
-// comecar a desenvolver aqui depois
-void tree_sort(int *vetor, int tamanho)
-{
-    (void)vetor;
-    (void)tamanho;
+typedef struct No {
+    int valor;
+    struct No *esq;
+    struct No *dir;
+} No;
 
-    printf("ok\n");
+No *criar_no(int valor) {
+    No *novo = (No *) malloc(sizeof(No));
+    novo->valor = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+void inserir_no(No **raiz, int valor) {
+    if (*raiz == NULL) {
+        *raiz = criar_no(valor);
+    }
+
+    else if (valor < (*raiz)->valor) {
+        inserir_no(&((*raiz)->esq), valor);
+    }
+
+    else {
+        inserir_no(&((*raiz)->dir), valor);
+    }
+}
+
+void inserir_todos(No **raiz, int *vetor, int indice, int tamanho) {
+    if (indice < tamanho) {
+        inserir_no(raiz, vetor[indice]);
+        inserir_todos(raiz, vetor, indice + 1, tamanho);
+    }
+}
+
+void em_ordem(No *raiz, int *vetor, int *indice) {
+    if (raiz != NULL) {
+        em_ordem(raiz->esq, vetor, indice);
+
+        vetor[*indice] = raiz->valor;
+        *indice = *indice + 1;
+
+        em_ordem(raiz->dir, vetor, indice);
+    }
+}
+
+void liberar_arvore(No *raiz) {
+    if (raiz != NULL) {
+        liberar_arvore(raiz->esq);
+        liberar_arvore(raiz->dir);
+        free(raiz);
+    }
+}
+
+void tree_sort(int *vetor, int tamanho) {
+    No *raiz = NULL;
+    int indice = 0;
+
+    inserir_todos(&raiz, vetor, 0, tamanho);
+    em_ordem(raiz, vetor, &indice);
+    liberar_arvore(raiz);
 }
